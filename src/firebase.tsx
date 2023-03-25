b4967043b4d9864c40d2c8841950db9f1c4457ac
Gabriel Penteado <gabripenteado@gmail.com>
2023-03-25 01:08:29 -0300

    deleted firebase hosting files

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpGoDCFDPaKve_UBydK4tta4N6QQ15UvM",
-  authDomain: "form-database-98e52.firebaseapp.com",
-  projectId: "form-database-98e52",
-  storageBucket: "form-database-98e52.appspot.com",
-  messagingSenderId: "1025964817523",
-  appId: "1:1025964817523:web:12be09f4e6a43eec263e06",
-  measurementId: "G-E39YB6PJ9P"
+  apiKey: "AIzaSyBcuFeTS9nS6ujr043NwkiQGl9YJQ9YwJM",
+  authDomain: "freedatabaseform.firebaseapp.com",
+  projectId: "freedatabaseform",
+  storageBucket: "freedatabaseform.appspot.com",
+  messagingSenderId: "750827951023",
+  appId: "1:750827951023:web:2f8437599abc3df61e555a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getDatabase(app);
